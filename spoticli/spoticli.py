09ce1d4f9d848ca15e023b7d8b3ef9641c65ba16
Joe Bonneau <joebonneau@gmail.com>
2021-08-07 09:14:02 -0700

    add add_current_track_to_playlists

--- spoticli/spoticli.py
+++ spoticli/spoticli.py
@@ -1,16 +1,26 @@
 import random
 import os
+import time
 
 import spotipy as sp
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 import click
 from click import style
+from tabulate import tabulate
 
 
-SCOPE = " user-read-playback-state"
 SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
+SPOTIFY_USER_ID = os.environ.get("SPOTIFY_USER_ID")
 SPOTIFY_REDIRECT_URI = os.environ.get("SPOTIFY_REDIRECT_URI")
+
+
+def convert_duration(duration_ms):
+
+    minutes, seconds = divmod(duration_ms / 1000, 60)
+    duration = f"{int(minutes)}:{int(round(seconds,0))}"
+
+    return duration
 
 
 def connect(
@@ -30,7 +40,8 @@ def connect(
         )
     except:
         click.secho(
-            "API authorization failed! Did you set the environment variables?", fg="red"
+            "API authorization failed! Did you remember to set the environment variables?",
+            fg="red",
         )
 
     return auth
@@ -39,47 +50,6 @@ def connect(
 @click.group()
 def main():
     pass
-
-
-# @main.command()
-# @click.option(
-#     "--clientid",
-#     "-ci",
-#     "client_id",
-#     required=True,
-#     type=str,
-#     envvar="SPOTIFY_CLIENT_ID",
-# )
-# @click.option(
-#     "--clientsecret",
-#     "-cs",
-#     "client_secret",
-#     required=True,
-#     type=str,
-#     envvar="SPOTIFY_CLIENT_SECRET",
-# )
-# @click.option(
-#     "--redirecturl",
-#     "-rd",
-#     "redirect_uri",
-#     required=True,
-#     type=str,
-#     envvar="SPOTIFY_REDIRECT_URI",
-# )
-# def connect(ctx, client_id, client_secret, redirect_uri):
-#     try:
-#         ctx.obj = sp.Spotify(
-#             auth_manager=SpotifyOAuth(
-#                 client_id=client_id,
-#                 client_secret=client_secret,
-#                 redirect_uri=redirect_uri,
-#                 open_browser=False,
-#                 scope=SCOPE,
-#             )
-#         )
-#         click.secho("API authorization successful!", fg="green")
-#     except:
-#         click.secho("API authorization failed!", fg="red")
 
 
 @main.command()
@@ -148,10 +118,17 @@ def play_random_saved_album():
 @main.command()
 def add_current_track_to_playlists():
 
-    sp_auth = connect()
+    sp_auth = connect(
+        scope="user-read-playback-state playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private"
+    )
 
-    current_playback = sp_auth.current_playback()
-    current_track = current_playback["item"]["uri"]
+    try:
+        current_playback = sp_auth.current_playback()
+    except TypeError:
+        click.secho("Nothing is currently playing!", fg="red")
+
+    current_track = current_playback["item"]["name"]
+    current_track_uri = current_playback["item"]["uri"]
     current_artists = []
     for i in range(len(current_playback["item"]["artists"])):
         current_artists.append(current_playback["item"]["artists"][i]["name"])
@@ -159,3 +136,47 @@ def add_current_track_to_playlists():
     click.echo(
         f"The current track playing is {style(current_track, fg='blue')} by {style(', '.join(current_artists), fg='green')}"
     )
+    duration = convert_duration(current_playback["item"]["duration_ms"])
+    release_date = current_playback["item"]["album"]["release_date"]
+    click.echo(f"Duration: {duration}, Released: {release_date}")
+
+    playlist_res = sp_auth.current_user_playlists(limit=20)
+    positions = []
+    playlist_names = []
+    playlist_ids = []
+    for i in range(len(playlist_res["items"])):
+        positions.append(i)
+        playlist_names.append(playlist_res["items"][i]["name"])
+        playlist_ids.append(playlist_res["items"][i]["uri"])
+
+    playlist_dict = {
+        "index": positions,
+        "playlist_names": playlist_names,
+        "playlist_ids": playlist_ids,
+    }
+    display_dict = {"index": positions, "playlist_names": playlist_names}
+    click.echo(tabulate(display_dict, headers="keys", tablefmt="github"))
+
+    while True:
+        indices = click.prompt(
+            "Enter the indices of the playlists to add the track to separated by commas."
+        )
+
+        try:
+            indices_int = [int(index) for index in indices.split(",")]
+
+            for index in indices_int:
+                sp_auth.user_playlist_add_tracks(
+                    user=SPOTIFY_USER_ID,
+                    playlist_id=playlist_dict.get("playlist_ids")[index],
+                    tracks=[current_track_uri],
+                )
+
+            click.secho(f"The track was successfully added to all specified playlists!")
+            break
+        except ValueError:
+            click.secho(f"Invalid input! Try again.", fg="red")
+        except:
+            click.secho(
+                f"There was an issue adding the track to all specified playlists."
+            )
