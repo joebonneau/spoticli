3f30704c7069b41588795925765d7cc29c50166a
Joe Bonneau <joebonneau@gmail.com>
2021-08-08 15:52:00 -0700

    add prev, next, pause, play, vol, np, shuffle

--- spoticli/spoticli.py
+++ spoticli/spoticli.py
@@ -1,55 +1,34 @@
 import random
 import os
-import time
 
 import spotipy as sp
-from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 import click
-from click import style
+from click.termui import style
 from tabulate import tabulate
 
+from spoticli.util import convert_duration, connect, get_current_playback
 
-SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
-SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
+
 SPOTIFY_USER_ID = os.environ.get("SPOTIFY_USER_ID")
-SPOTIFY_REDIRECT_URI = os.environ.get("SPOTIFY_REDIRECT_URI")
 
+USER_MODIFY_PLAYBACK_STATE = "user-modify-playback-state"
 
-def convert_duration(duration_ms):
+states = [
+    "user-modify-playback-state",
+    "user-library-read",
+    "playlist-read-private",
+    "playlist-read-collaborative",
+    "playlist-modify-public",
+    "playlist-modify-private",
+]
 
-    minutes, seconds = divmod(duration_ms / 1000, 60)
-    duration = f"{int(minutes)}:{int(round(seconds,0))}"
-
-    return duration
-
-
-def connect(
-    scope,
-    client_id=SPOTIFY_CLIENT_ID,
-    client_secret=SPOTIFY_CLIENT_SECRET,
-    redirect_uri=SPOTIFY_REDIRECT_URI,
-):
-    try:
-        auth = sp.Spotify(
-            auth_manager=SpotifyOAuth(
-                scope=scope,
-                client_id=client_id,
-                client_secret=client_secret,
-                redirect_uri=redirect_uri,
-            )
-        )
-    except:
-        click.secho(
-            "API authorization failed! Did you remember to set the environment variables?",
-            fg="red",
-        )
-
-    return auth
+STATE_STR = " ".join(states)
 
 
 @click.group()
 def main():
     pass
+    # TODO: Figure out how to check whether a valid token exists and connect if not
 
 
 @main.command()
@@ -105,13 +84,15 @@ def play_random_saved_album():
         if queue.lower() not in ["p", "q"]:
             click.secho("Invalid input!", fg="red")
         elif queue.lower() == "q":
-            tracks_res = sp_auth.album_tracks(selected_album, limit=50)
+            tracks_res = sp_auth.album_tracks(
+                saved_albums[rand_i]["album_uri"], limit=50
+            )
             tracks = tracks_res["items"]
             for i in range(len(tracks)):
                 sp_auth.add_to_queue(tracks_res["items"][i]["uri"])
             break
         else:
-            sp_auth.start_playback(context_uri=saved_albums[rand_i].get("album_uri"))
+            sp_auth.start_playback(context_uri=saved_albums[rand_i]["album_uri"])
             break
 
 
@@ -123,60 +104,134 @@ def add_current_track_to_playlists():
     )
 
     try:
-        current_playback = sp_auth.current_playback()
+        playback = get_current_playback(display=True)
+
+        playlist_res = sp_auth.current_user_playlists(limit=20)
+        positions = []
+        playlist_names = []
+        playlist_ids = []
+        for i in range(len(playlist_res["items"])):
+            positions.append(i)
+            playlist_names.append(playlist_res["items"][i]["name"])
+            playlist_ids.append(playlist_res["items"][i]["uri"])
+
+        playlist_dict = {
+            "index": positions,
+            "playlist_names": playlist_names,
+            "playlist_ids": playlist_ids,
+        }
+        display_dict = {"index": positions, "playlist_names": playlist_names}
+        click.echo(tabulate(display_dict, headers="keys", tablefmt="github"))
+
+        while True:
+            indices = click.prompt(
+                "Enter the indices of the playlists to add the track to separated by commas."
+            )
+
+            try:
+                indices_int = list(map(int, indices.split(",")))
+
+                for index in indices_int:
+                    sp_auth.user_playlist_add_tracks(
+                        user=SPOTIFY_USER_ID,
+                        playlist_id=playlist_dict["playlist_ids"][index],
+                        tracks=[playback["track_uri"]],
+                    )
+
+                click.secho(
+                    f"The track was successfully added to all specified playlists!"
+                )
+                break
+            except ValueError:
+                click.secho(f"Invalid input! Try again.", fg="red")
+            # except:
+            #     click.secho(
+            #         f"There was an issue adding the track to all specified playlists."
+            #     )
     except TypeError:
         click.secho("Nothing is currently playing!", fg="red")
 
-    current_track = current_playback["item"]["name"]
-    current_track_uri = current_playback["item"]["uri"]
-    current_artists = []
-    for i in range(len(current_playback["item"]["artists"])):
-        current_artists.append(current_playback["item"]["artists"][i]["name"])
 
-    click.echo(
-        f"The current track playing is {style(current_track, fg='blue')} by {style(', '.join(current_artists), fg='green')}"
-    )
-    duration = convert_duration(current_playback["item"]["duration_ms"])
-    release_date = current_playback["item"]["album"]["release_date"]
-    click.echo(f"Duration: {duration}, Released: {release_date}")
+@main.command("prev")
+def previous_track():
+    sp_auth = connect(scope=USER_MODIFY_PLAYBACK_STATE)
+    sp_auth.previous_track()
 
-    playlist_res = sp_auth.current_user_playlists(limit=20)
-    positions = []
-    playlist_names = []
-    playlist_ids = []
-    for i in range(len(playlist_res["items"])):
-        positions.append(i)
-        playlist_names.append(playlist_res["items"][i]["name"])
-        playlist_ids.append(playlist_res["items"][i]["uri"])
+    get_current_playback(display=True)
 
-    playlist_dict = {
-        "index": positions,
-        "playlist_names": playlist_names,
-        "playlist_ids": playlist_ids,
-    }
-    display_dict = {"index": positions, "playlist_names": playlist_names}
-    click.echo(tabulate(display_dict, headers="keys", tablefmt="github"))
 
-    while True:
-        indices = click.prompt(
-            "Enter the indices of the playlists to add the track to separated by commas."
-        )
+@main.command("next")
+def next_track():
+    sp_auth = connect(scope=USER_MODIFY_PLAYBACK_STATE)
+    sp_auth.next_track()
 
-        try:
-            indices_int = [int(index) for index in indices.split(",")]
+    get_current_playback(display=True)
 
-            for index in indices_int:
-                sp_auth.user_playlist_add_tracks(
-                    user=SPOTIFY_USER_ID,
-                    playlist_id=playlist_dict.get("playlist_ids")[index],
-                    tracks=[current_track_uri],
-                )
 
-            click.secho(f"The track was successfully added to all specified playlists!")
-            break
-        except ValueError:
-            click.secho(f"Invalid input! Try again.", fg="red")
-        except:
-            click.secho(
-                f"There was an issue adding the track to all specified playlists."
-            )
+@main.command("pause")
+def pause_playback():
+    sp_auth = connect(scope=USER_MODIFY_PLAYBACK_STATE)
+    sp_auth.pause_playback()
+
+    click.secho("Playback paused.")
+
+
+@main.command("play")
+def start_playback():
+    sp_auth = connect(scope=USER_MODIFY_PLAYBACK_STATE)
+    sp_auth.start_playback()
+
+    click.secho("Playback resumed.")
+    get_current_playback(display=True)
+
+
+@main.command("vol")
+@click.option("-u/-d", "--up/--down", required=True)
+def volume(up):
+
+    sp_auth = connect(scope=USER_MODIFY_PLAYBACK_STATE)
+    playback_info = get_current_playback(display=False)
+
+    if up:
+        previous_volume = playback_info["volume"]
+        new_volume = int(round(previous_volume + 10, 0))
+    else:
+        previous_volume = playback_info["volume"]
+        new_volume = int(round(previous_volume - 10, 0))
+
+    if new_volume > 100:
+        new_volume = 100
+    elif new_volume < 0:
+        new_volume = 0
+
+    sp_auth.volume(new_volume)
+
+    click.secho(f"Current volume: {new_volume}")
+
+
+@main.command("np")
+# @click.option("-v", "--verbose")
+def now_playing():
+
+    get_current_playback(display=True)
+
+    # TODO: add verbose option to display audio features
+
+
+@main.command("shuffle")
+@click.option("-on/-off", default=True)
+def toggle_shuffle(on):
+
+    sp_auth = connect(scope=USER_MODIFY_PLAYBACK_STATE)
+
+    if on:
+        sp_auth.shuffle(state=True)
+        click.echo(f"Shuffle toggled {style('on', fg='green')}.")
+
+    else:
+        sp_auth.shuffle(state=False)
+        click.echo(f"Shuffle toggled {style('off', fg='red')}.")
+
+
+# TODO: add currently_playing (with more info),
+# TODO: recently_played, smart searching, shuffle
