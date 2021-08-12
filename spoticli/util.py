817ff0b713c0838506f2e06535c503da4b365950
Joe Bonneau <68512346+joebonneau@users.noreply.github.com>
2021-08-12 04:02:52 +0000

    Spoticli error handling docs (#7)
    * add docstrings, help strings; clean up user prompt logic
    
    * add notes for AttributeError handling
    
    * update README and add more error handling

--- spoticli/util.py
+++ spoticli/util.py
@@ -5,14 +5,18 @@ import spotipy as sp
 import click
 from click.termui import style
 
-SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
-SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
-SPOTIFY_REDIRECT_URI = os.environ.get("SPOTIFY_REDIRECT_URI")
+# SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
+# SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
+# SPOTIFY_REDIRECT_URI = os.environ.get("SPOTIFY_REDIRECT_URI")
 
-USER_READ_PLAYBACK_STATE = "user-read-playback-state"
+# USER_READ_PLAYBACK_STATE = "user-read-playback-state"
 
 
 def get_current_playback(sp_auth: sp.Spotify, display: bool) -> dict:
+    """
+    Retrieves current playback information, parses the json response, and optionally displays
+    information about the current playback.
+    """
 
     current_playback = sp_auth.current_playback()
     playback_items = current_playback["item"]
@@ -27,6 +31,7 @@ def get_current_playback(sp_auth: sp.Spotify, display: bool) -> dict:
     playback["track_name"] = playback_items["name"]
     playback["track_uri"] = playback_items["uri"]
     playback["album_name"] = playback_items["album"]["name"]
+    playback["album_type"] = playback_items["album"]["type"]
     playback["album_uri"] = playback_items["album"]["uri"]
     playback["release_date"] = playback_items["album"]["release_date"]
     playback["duration"] = convert_ms(playback_items["duration_ms"])
@@ -37,9 +42,10 @@ def get_current_playback(sp_auth: sp.Spotify, display: bool) -> dict:
         track_name = style(playback["track_name"], fg="magenta")
         artists_name = style(playback["artists"], fg="green")
         album_name = style(playback["album_name"], fg="blue")
+        album_type = playback["album_type"]
 
         click.secho(
-            f"Now playing: {track_name} by {artists_name} from the album {album_name}"
+            f"Now playing: {track_name} by {artists_name} from the {album_type} {album_name}"
         )
         click.echo(
             f"Duration: {playback['duration']}, Released: {playback['release_date']}"
@@ -49,11 +55,14 @@ def get_current_playback(sp_auth: sp.Spotify, display: bool) -> dict:
 
 
 def convert_ms(duration_ms: int) -> str:
+    """
+    Converts milliseconds to a string representation of a timestamp (MM:SS).
+    """
 
     minutes, seconds = divmod(duration_ms / 1000, 60)
     rounded_seconds = int(round(seconds, 0))
 
-    if rounded_seconds - 10 <= 0:
+    if rounded_seconds - 10 < 0:
         rounded_seconds = "0" + str(rounded_seconds)
 
     duration = f"{int(minutes)}:{rounded_seconds}"
@@ -62,6 +71,9 @@ def convert_ms(duration_ms: int) -> str:
 
 
 def convert_timestamp(timestamp: str) -> int:
+    """
+    Converts a timestamp (MM:SS) to milliseconds.
+    """
 
     timestamp_list = timestamp.split(":")
     minutes = timestamp_list[0]
@@ -70,9 +82,9 @@ def convert_timestamp(timestamp: str) -> int:
     seconds_in_ms = int(seconds) * 1000
     total_ms = minutes_in_ms + seconds_in_ms
 
-    if len(seconds) > 2 or len(seconds) < 2:
-        raise ValueError
-    elif minutes_in_ms < 0 or seconds_in_ms < 0:
+    if (len(seconds) > 2 or len(seconds) < 2) or (
+        minutes_in_ms < 0 or seconds_in_ms < 0
+    ):
         raise ValueError
     else:
         return total_ms
