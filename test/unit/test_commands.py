76503ea6070ba855c4126af673d985422aae4e5b
Joe Bonneau <joebonneau@gmail.com>
2021-08-16 20:21:59 -0700

    set up pre-commit hook

---
+++ test/unit/test_commands.py
@@ -0,0 +1,16 @@
+from unittest.mock import Mock, patch
+
+from click.testing import CliRunner
+
+from spoticli.spoticli import main
+
+
+# Unit Testing
+
+
+@patch("spoticli.spoticli.requests.get")
+def test_previous():
+    runner = CliRunner()
+    result = runner.invoke(main)
+    print(result.output)
+    assert "API authorization failed!" not in result.output
